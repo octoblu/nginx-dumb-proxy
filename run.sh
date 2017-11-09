1ec9592e5d3f746273f3f5cef7eef4f96b24a59f
Roy van de Water <roy.vwater@gmail.com>
2017-11-09 16:07:12 -0700

    v2.2.0 Use local /etc/resolv.conf

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 alter_the_conf(){
   local target_protocol="$1"
   local target_hostname="$2"
+  local resolvers="$3"
 
   # using delimiter '|' instead of '/' to prevent problems with
   # path delimiter 'foo.com/whatever'
@@ -15,6 +16,11 @@ alter_the_conf(){
     --in-place='' \
     --expression="s|target_hostname|$target_hostname|g" \
     /etc/nginx/nginx.conf
+
+  sed \
+    --in-place='' \
+    --expression="s|# RESOLVERS_HOOK|$resolvers|g" \
+    /etc/nginx/nginx.conf
 }
 
 assert_env(){
@@ -22,6 +28,12 @@ assert_env(){
     echo 'Missing required env: NGINX_DUMB_PROXY_TARGET_HOSTNAME' 2>&1
     exit 1
   fi
+}
+
+get_resolvers() {
+  cat /etc/resolv.conf \
+  | grep '^nameserver' \
+  | awk '{print "resolver "$2";"}'
 }
 
 run_the_server(){
@@ -32,15 +44,16 @@ run_the_server(){
 main(){
   assert_env
 
-  local target_hostname target_protocol target_url
+  local resolvers target_hostname target_protocol target_url
 
+  resolvers="$(get_resolvers)"
   target_hostname="${NGINX_DUMB_PROXY_TARGET_HOSTNAME:-$TARGET_HOSTNAME}"
   target_protocol="${NGINX_DUMB_PROXY_TARGET_PROTOCOL:-${TARGET_PROTOCOL:-https}}"
   target_url="${target_protocol}://${target_hostname}"
 
   echo "Redirecting all traffic to '$target_url'"
 
-  alter_the_conf "$target_protocol" "$target_hostname" \
+  alter_the_conf "$target_protocol" "$target_hostname" "${resolvers[@]}" \
   && run_the_server
 }
 main "$@"
